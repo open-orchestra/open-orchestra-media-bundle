567a80de3882a2d67c963e02ad8eb56969f07acf
Nicolas Thal <nico.th4l@gmail.com>
2014-08-25 10:10:41 +0200

    add other websites

---
+++ ModelBundle/DataFixtures/MongoDB/LoadContentTypeData.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PHPOrchestra\ModelBundle\DataFixtures\MongoDB;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Class LoadContentTypeData
+ */
+class LoadContentTypeData implements FixtureInterface
+{
+    /**
+     * @param ObjectManager $manager
+     */
+    function load(ObjectManager $manager)
+    {
+        // TODO: Implement load() method.
+    }
+
+}
