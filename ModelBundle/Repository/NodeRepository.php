124f24ea475e32d4480c9c7694a88613fa530369
Nicolas Thal <nico.th4l@gmail.com>
2014-09-12 11:41:25 +0200

    factorise status search

--- ModelBundle/Repository/NodeRepository.php
+++ ModelBundle/Repository/NodeRepository.php
@@ -17,17 +17,8 @@ class NodeRepository extends DocumentRepository
      */
     public function getFooterTree()
     {
-        $dm = $this->getDocumentManager();
-        $statuses = $dm->getRepository('PHPOrchestra\ModelBundle\Document\Status')->findBy(array('published' => true));
-
-        $qb = $this->createQueryBuilder('n');
-
-        foreach ($statuses as $status) {
-            $qb->field('status')->references($status);
-        }
-
-        $qb->field('deleted')->equals(false)
-            ->field('inFooter')->equals(true);
+        $qb = $this->buildTreeRequest();
+        $qb->field('inFooter')->equals(true);
 
         return $qb->getQuery()->execute();
     }
@@ -37,17 +28,8 @@ class NodeRepository extends DocumentRepository
      */
     public function getMenuTree()
     {
-        $dm = $this->getDocumentManager();
-        $statuses = $dm->getRepository('PHPOrchestra\ModelBundle\Document\Status')->findBy(array('published' => true));
-
-        $qb = $this->createQueryBuilder('n');
-
-        foreach ($statuses as $status) {
-            $qb->field('status')->references($status);
-        }
-
-        $qb->field('deleted')->equals(false)
-            ->field('inMenu')->equals(true);
+        $qb = $this->buildTreeRequest();
+        $qb->field('inMenu')->equals(true);
 
         return $qb->getQuery()->execute();
     }
@@ -92,5 +74,23 @@ class NodeRepository extends DocumentRepository
         }
 
         return null;
+    }
+
+    /**
+     * @return \Doctrine\ODM\MongoDB\Query\Builder
+     */
+    protected function buildTreeRequest()
+    {
+        $dm = $this->getDocumentManager();
+        $statuses = $dm->getRepository('PHPOrchestra\ModelBundle\Document\Status')->findBy(array('published' => true));
+
+        $qb = $this->createQueryBuilder('n');
+
+        foreach ($statuses as $status) {
+            $qb->addOr($qb->expr()->field('status')->references($status));
+        }
+
+        $qb->field('deleted')->equals(false);
+        return $qb;
     }
 }
