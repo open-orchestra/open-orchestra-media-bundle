dbef0646c6aa6e38f28f1cf40472b6049a8c9848
itkg-ngilain <noel.gilain@businessdecision.com>
2014-11-18 16:05:03 +0100

    Remove useless themes

--- ModelBundle/DataFixtures/MongoDB/LoadThemeData.php
+++ ModelBundle/DataFixtures/MongoDB/LoadThemeData.php
@@ -17,26 +17,6 @@ class LoadThemeData implements FixtureInterface
     public function load(ObjectManager $manager)
     {
         $theme = new Theme();
-        $theme->setName('theme1');
-        $manager->persist($theme);
-
-        $theme = new Theme();
-        $theme->setName('theme2');
-        $manager->persist($theme);
-
-        $theme = new Theme();
-        $theme->setName('sample');
-        $manager->persist($theme);
-
-        $theme = new Theme();
-        $theme->setName('fromApp');
-        $manager->persist($theme);
-
-        $theme = new Theme();
-        $theme->setName('mixed');
-        $manager->persist($theme);
-
-        $theme = new Theme();
         $theme->setName('themePresentation');
         $manager->persist($theme);
 
