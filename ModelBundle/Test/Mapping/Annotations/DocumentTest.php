3403d93e5963b4343bc64fd55952c189545a0d49
Nicolas ANNE <nicolas.anne@businessdecision.com>
2014-11-27 16:42:31 +0100

    PR corrections

---
+++ ModelBundle/Test/Mapping/Annotations/DocumentTest.php
@@ -0,0 +1,137 @@
+<?php
+namespace PHPOrchestra\ModelBundle\Test\Mapping\Annotations;
+
+use Phake;
+use PHPOrchestra\ModelBundle\Mapping\Annotations\Document;
+use PHPOrchestra\ModelBundle\Model\NodeInterface;
+/**
+ * Class DocumentTest
+ */
+class DocumentTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $node;
+
+    /**
+     * setUp
+     */
+    public function setUp()
+    {
+        $this->node = Phake::mock('PHPOrchestra\ModelBundle\Model\NodeInterface');
+    }
+
+/*    public function testInitRepository(){
+        $document->initRepository();
+    }
+    public function testExists(){
+        $document->exists($id);
+    }*/
+
+    /**
+     * @param array         $parameters
+     * @param string        $exception
+     * @param string        $expectedResult
+     *
+     * @dataProvider provideSource
+     */
+    public function testGetSource($parameters, $exception, $expectedResult){
+        $document = new Document($parameters);
+        $source = $document->getSource($this->node);
+        if($exception){
+            $this->setExpectedException($exception);
+        }
+        else{
+            $this->assertSame($source, $expectedResult);
+        }
+    }
+
+    /**
+     * @param array         $parameters
+     * @param string        $exception
+     * @param string        $expectedResult
+     *
+     * @dataProvider provideGetGenerated
+     */
+    public function testGetGenerated($parameters, $exception, $expectedResult){
+        $document = new Document($parameters);
+        $generate = $document->getGenerated($this->node);
+        if($exception){
+            $this->setExpectedException($exception);
+        }
+        else{
+            $this->assertSame($generate, $expectedResult);
+        }
+    }
+
+    /**
+     * @param array         $parameters
+     * @param string        $exception
+     * @param string        $expectedResult
+     *
+     * @dataProvider provideSetGenerated
+     */
+    public function testSetGenerated($parameters, $exception, $expectedResult){
+        $document = new Document($parameters);
+        $generate = $document->setGenerated($this->node);
+        if($exception){
+            $this->setExpectedException($exception);
+        }
+        else{
+            $this->assertSame($generate, $expectedResult);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function provideSource()
+    {
+        $parameters0 = array('sourceId' => 'name');
+
+        $parameters1 = array();
+
+        $parameters2 = array('sourceId' => 'fakeproperty');
+
+        return array(
+            array($parameters0, null, 'getName'),
+            array($parameters1, 'PropertyNotFoundException', null),
+            array($parameters2, 'MethodNotFoundException', null),
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function provideGetGenerated()
+    {
+        $parameters0 = array('generatedId' => 'nodeId');
+
+        $parameters1 = array();
+
+        $parameters2 = array('generatedId' => 'fakeproperty');
+
+        return array(
+            array($parameters0, null, 'getNodeId'),
+            array($parameters1, 'PropertyNotFoundException', null),
+            array($parameters2, 'MethodNotFoundException', null),
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function provideSetGenerated()
+    {
+        $parameters0 = array('generatedId' => 'nodeId');
+
+        $parameters1 = array();
+
+        $parameters2 = array('generatedId' => 'fakeproperty');
+
+        return array(
+            array($parameters0, null, 'setNodeId'),
+            array($parameters1, 'PropertyNotFoundException', null),
+            array($parameters2, 'MethodNotFoundException', null),
+        );
+    }
+}
