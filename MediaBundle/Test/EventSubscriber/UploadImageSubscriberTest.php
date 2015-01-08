d9f364fde1c478271fb28c81c48ae6c3fd7d811f
itkg-ngilain <noel.gilain@businessdecision.com>
2015-01-08 16:52:53 +0100

    Add UploadImageSubscriberTest

---
+++ MediaBundle/Test/EventSubscriber/UploadImageSubscriberTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace PHPOrchestra\MediaBundle\Test\EventSubscriber;
+
+use Phake;
+use PHPOrchestra\Media\EventSubscriber\UploadImageSubscriber;
+use PHPOrchestra\Media\MediaEvents;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Class UploadImageSubscriberTest
+ */
+class UploadImageSubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    protected $subscriber;
+    protected $gaufretteManager;
+    protected $event;
+
+    /**
+     * Set up the test
+     */
+    public function setUp()
+    {
+        $this->gaufretteManager = Phake::mock('PHPOrchestra\Media\Manager\GaufretteManager');
+
+        $this->subscriber = new UploadImageSubscriber($this->gaufretteManager);
+
+        $this->event = Phake::mock('PHPOrchestra\Media\Event\ResizeImageEvent');
+    }
+
+    /**
+     * test instance
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf('Symfony\Component\EventDispatcher\EventSubscriberInterface', $this->subscriber);
+    }
+
+    /**
+     * Test event subscribed
+     */
+    public function testEventSubscribed()
+    {
+        $this->assertArrayHasKey(MediaEvents::RESIZE_IMAGE, $this->subscriber->getSubscribedEvents());
+    }
+
+    /**
+     * Test methodes existance
+     */
+    public function testMethodExists()
+    {
+        $this->assertTrue(method_exists($this->subscriber, 'uploadImage'));
+    }
+
+    /**
+     * @param string $filename
+     * @param string $fileContent
+     *
+     * @dataProvider provideFileInfos
+     */
+    public function testUploadImage($filename, $fileContent)
+    {
+        Phake::when($this->event)->getFileName()->thenReturn($filename);
+        Phake::when($this->event)->getFileContent()->thenReturn($fileContent);
+
+        $this->subscriber->uploadImage($this->event);
+
+        Phake::verify($this->gaufretteManager, Phake::times(1))->uploadContent($filename, $fileContent);
+    }
+
+    public function provideFileInfos()
+    {
+        return array(
+            array('someName', 'someContent')
+        );
+    }
+}
