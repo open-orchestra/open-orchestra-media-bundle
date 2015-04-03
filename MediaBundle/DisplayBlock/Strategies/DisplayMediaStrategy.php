35023bc733914c48efd3d8dadae9a7de9a9b75ef
itkg-bfouche <benjamin.fouche@businessdecision.com>
2015-04-03 14:28:52 +0200

    add display media block

---
+++ MediaBundle/DisplayBlock/Strategies/DisplayMediaStrategy.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace OpenOrchestra\MediaBundle\DisplayBlock\Strategies;
+
+use OpenOrchestra\DisplayBundle\DisplayBlock\Strategies\AbstractStrategy;
+use OpenOrchestra\ModelInterface\Model\ReadBlockInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class DisplayMediaStrategy
+ */
+class DisplayMediaStrategy extends AbstractStrategy
+{
+    const DISPLAY_MEDIA = "display_media";
+
+    /**
+     * Check if the strategy support this block
+     *
+     * @param ReadBlockInterface $block
+     *
+     * @return boolean
+     */
+    public function support(ReadBlockInterface $block)
+    {
+        return self::DISPLAY_MEDIA == $block->getComponent();
+    }
+
+    /**
+     * Perform the show action for a block
+     *
+     * @param ReadBlockInterface $block
+     *
+     * @return Response
+     */
+    public function show(ReadBlockInterface $block)
+    {
+
+    }
+
+    /**
+     * Get the name of the strategy
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return "display_media";
+    }
+}
