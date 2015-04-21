54bc088f4b6616bfb78df45d094b350712e9aca2
Benjamin Fouché <benjamin.fouche@businessdecision.com>
2015-04-21 10:11:11 +0200

    delete media with gaufrette

--- Media/Manager/GaufretteManager.php
+++ Media/Manager/GaufretteManager.php
@@ -25,6 +25,8 @@ class GaufretteManager
      * 
      * @param string $key
      * @param string $filecontent
+     *
+     * @return integer|boolean The number of bytes that were written into the file
      */
     public function uploadContent($key, $filecontent)
     {
@@ -35,10 +37,31 @@ class GaufretteManager
      * Get content of a file with gaufrette
      * 
      * @param string $key
+     *
+     * @return string|boolean if cannot read content
      */
     public function getFileContent($key)
     {
         return $this->adapter->read($key);
     }
 
+    /**
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function deleteContent($key)
+    {
+        return $this->adapter->delete($key);
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function exists($key)
+    {
+        return $this->adapter->exists($key);
+    }
 }
---
+++ MediaBundle/Tests/EventSubscriber/DeleteMediaSubscriberTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace OpenOrchestra\MediaBundle\Tests\EventSubscriber;
+
+use OpenOrchestra\Media\EventSubscriber\DeleteMediaSubscriber;
+use OpenOrchestra\Media\MediaEvents;
+use Phake;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Class DeleteMediaSubscriberTest
+ */
+class DeleteMediaSubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    protected $subscriber;
+
+    protected $formats = array('max-height' => 100, 'max-width' => 100);
+    protected $gaufrette;
+    protected $event;
+    protected $media;
+
+    /**
+     * Set Up the test
+     */
+    public function setUp()
+    {
+        $this->media = Phake::mock('OpenOrchestra\Media\Model\MediaInterface');
+
+        $this->event = Phake::mock('OpenOrchestra\Media\Event\MediaEvent');
+        Phake::when($this->event)->getMedia()->thenReturn($this->media);
+
+        $this->gaufrette = Phake::mock('OpenOrchestra\Media\Manager\GaufretteManager');
+        $this->subscriber = new DeleteMediaSubscriber($this->gaufrette, $this->formats);
+    }
+
+    /**
+     * test instance
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf('Symfony\Component\EventDispatcher\EventSubscriberInterface', $this->subscriber);
+    }
+
+    /**
+     * Test event subscribed
+     */
+    public function testEventSubscribed()
+    {
+        $this->assertArrayHasKey(MediaEvents::MEDIA_DELETE, $this->subscriber->getSubscribedEvents());
+        $this->assertArrayHasKey(KernelEvents::TERMINATE, $this->subscriber->getSubscribedEvents());
+    }
+
+    /**
+     * Test if method exists
+     */
+    public function testMethodExists()
+    {
+        $this->assertTrue(method_exists($this->subscriber, 'deleteMedia'));
+        $this->assertTrue(method_exists($this->subscriber, 'removeMedias'));
+    }
+
+    /**
+     * @param string $name
+     * @param string $thumbnail
+     * @param int    $count
+     * @param bool   $exist
+     *
+     * @dataProvider generateMedia
+     */
+    public function testDeleteMedia($name, $thumbnail, $count, $exist)
+    {
+        Phake::when($this->media)->getFilesystemName()->thenReturn($name);
+        Phake::when($this->media)->getThumbnail()->thenReturn($thumbnail);
+
+        foreach ($this->formats as $key => $format) {
+            $formatName = $key . '-' . $name;
+            Phake::when($this->gaufrette)->exists($formatName)->thenReturn($exist);
+        }
+
+
+        $this->subscriber->deleteMedia($this->event);
+
+        Phake::verify($this->gaufrette, Phake::times(2))->exists(Phake::anyParameters());
+
+        $this->subscriber->removeMedias();
+
+        Phake::verify($this->gaufrette, Phake::times($count))->deleteContent(Phake::anyParameters());
+    }
+
+    /**
+     * @return array
+     */
+    public function generateMedia()
+    {
+        return array(
+            array('image1.jpg.jpg', 'image1.jpg.jpg', 3, true),
+            array('pdf1.pdf.pdf', 'pdf1.jpg.jpg', 2, false),
+            array('video1.3gp.3gp', 'video1.jpg.jpg', 2, false),
+        );
+    }
+}
