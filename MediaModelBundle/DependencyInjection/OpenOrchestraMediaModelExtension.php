c34589110b5c8d4a07e2dca9998f00588531f42e
Thibault Lefrançois <thi.lefrancois@gmail.com>
2016-02-19 18:35:36 +0100

    move listeners into MediaAdminModelBundle

--- MediaModelBundle/DependencyInjection/OpenOrchestraMediaModelExtension.php
+++ MediaModelBundle/DependencyInjection/OpenOrchestraMediaModelExtension.php
@@ -23,7 +23,6 @@ class OpenOrchestraMediaModelExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-        $loader->load('listener.yml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
