f7a27d327152a9913ac36fe7dc14651d3317ac47
Amaury Lavieille <amaury.lavieille@gmail.com>
2016-09-27 17:29:12 +0200

    update deprecated annotation (#235)

--- Media/Repository/FolderRepositoryInterface.php
+++ Media/Repository/FolderRepositoryInterface.php
@@ -11,7 +11,7 @@ use OpenOrchestra\Media\Model\FolderInterface;
 interface FolderRepositoryInterface
 {
     /**
-     * @deprecated FindAllRootFolder is deprecated since version 1.1.0 and will be removed in 1.3.0. use findAllRootFolderBySiteId
+     * @deprecated FindAllRootFolder is deprecated since version 1.1.0 and will be removed in 2.0. use findAllRootFolderBySiteId
      *
      * @return Collection
      */
